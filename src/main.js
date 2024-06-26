741889c84cd36a51ba5e03893275176b3a26d1cb
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-26 22:55:14 +0530

    6. functions that are needed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 /////////////////////////////////////
+// need 6 functions
+function getNotes() {
 
+}
 
+function saveNotes() {
 
+}
 
+function createNoteElement(id, content) {
 
+}
 
+function addNote() {
 
+}
 
+function update(id, newContent) {
+
+}
+
+function deleteNote() {
+    
+}
 
 
 
