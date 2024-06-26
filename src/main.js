48561cec8b6f5acf5325879256130d7c05ed51ef
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-26 23:15:58 +0530

    to get and save notes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 /////////////////////////////////////
 // need 6 functions
-function getNotes() {
+const notesContainer = document.getElementById("app");
+const addNoteButton = document.querySelector(".add-note");
 
+// to get notes
+function getNotes() {
+    return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
 }
 
-function saveNotes() {
-
+function saveNotes(notes) {
+    localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
 }
 
 function createNoteElement(id, content) {
@@ -21,8 +25,9 @@ function update(id, newContent) {
 }
 
 function deleteNote() {
-    
+
 }
+
 
 
 
